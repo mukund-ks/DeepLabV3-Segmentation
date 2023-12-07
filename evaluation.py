e577ee2674738f85453048266533051194078290
Mukund Kumar <sankhyan.mukund@gmail.com>
2023-12-07 18:13:59 +0530

    Fix y_pred & update gitignore

--- evaluation.py
+++ evaluation.py
@@ -74,8 +74,8 @@ def evaluator(eval_dir: str) -> None:
 
         y_pred = model.predict(x)[0]
         y_pred = np.squeeze(y_pred, axis=-1)
-        y_pred = (y_pred > 0).astype(np.int32)
-
+        y_pred = (y_pred > 0.5).astype(np.int32)
+        
         diagonal_len, horizontal_len, vertical_len = getMaskLen(y_pred)
 
         save_img_path = f"./eval_results/{name}.png"
